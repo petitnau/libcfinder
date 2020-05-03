60fdc0ef584913c29f5a04f49264c0c8c93cb1e8
petitnau <roberto.pettinau99@gmail.com>
2020-05-03 22:34:32 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name='libcfinder',
-    version='1.0.2',
+    version='1.0.3',
     scripts=[],
     author='Roberto Pettinau',
     author_email='roberto.pettinau99@gmail.com',
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/petitnau/libcfinder',
     packages=setuptools.find_packages(),
+      install_requires=[
+          'beautifulsoup4', 'requests'
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Topic :: Security"
